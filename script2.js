39ff5dca527f5047a9ee7275930783964dddc682
Baymanov Namat <namataxvd@gmail.com>
2021-12-22 15:48:06 +0600

    add js

---
+++ script2.js
@@ -0,0 +1,26 @@
+// var apple = 15;
+// var pear = 8;
+// if (apple <= pear) {
+//   alert("You guessed");
+// }
+// else {
+//   alert("you didn't guess");
+//}
+
+
+// var vanay = 78;
+// var colay = 8;
+// if (vanay <= colay) {
+//   alert("it is true");
+// }
+// else {
+//   alert("it is not true");
+// }
+var manay = 11;
+var jony = 11;
+if (manay + jony === 22) {
+  alert("true");
+}
+else {
+  alert("false");
+}
