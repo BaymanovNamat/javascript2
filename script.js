dbb8ac9785edccd059fe8f3f6ea431121846aeb7
Baymanov Namat <namataxvd@gmail.com>
2021-12-22 15:47:41 +0600

    add js

---
+++ script.js
@@ -0,0 +1,26 @@
+var example1 = 10 === 10;
+var example2 = 11 > 10;
+var example3 = 10 < 11;
+var example4 = 10 >= 10;
+var example5 = 10 <= 10;
+var example6 = 11 !== 10; // not equals
+
+
+var a = 15;
+var b = 10;
+var example7 = a < b;
+
+var c = 10;
+var d = 11;
+if (c < d) {
+  alert("C is less than D")
+}
+
+
+var e = 3.14;
+if (e !== 3.14) {
+  alert("why don't you know number PI?");
+}
+else {
+  alert("Good job!")
+}
