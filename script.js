1525698fe3e900b82dacfee0dcb99cdff18acfde
Baymanov Namat <namataxvd@gmail.com>
2021-12-27 15:31:24 +0600

    add html

--- script.js
+++ script.js
@@ -1,26 +1,102 @@
-var example1 = 10 === 10;
-var example2 = 11 > 10;
-var example3 = 10 < 11;
-var example4 = 10 >= 10;
-var example5 = 10 <= 10;
+var example1 = 10 === 10; // equals
+var example2 = 11 > 10; // more than
+var example3 = 10 < 11; // less than 
+var example4 = 10 >= 10; // more than or equal (it is true)
+var example5 = 10 <= 10; // less than or equal
 var example6 = 11 !== 10; // not equals
 
+// numbers 
+// 1, 2, 3, 4, 5, 6, 10.2
+var number1 = 1;
+var number2 = 2;
+var number3 = 3;
 
-var a = 15;
+// string, text
+var string1 = "Hello world";
+var string2 = "A";
+var string3 = "";
+var sting4 = "5";
+
+// boolean
+var boolean1 = true;
+var boolean2 = false;
+
+
+
+
+var a = 5;
 var b = 10;
 var example7 = a < b;
+console.log(example7);
+
 
 var c = 10;
 var d = 11;
 if (c < d) {
-  alert("C is less than D")
+ console.log("C is less than D")
 }
 
 
 var e = 3.14;
-if (e !== 3.14) {
-  alert("why don't you know number PI?");
+if (e !== 3.14) { // false
+ console.log("why don't you know figure PI?");
+}
+else { // true
+ console.log("Good job!");
+}
+
+
+
+var age = 10;
+if (age >= 18) {
+  console.log("You can vote!"); // true
 }
 else {
-  alert("Good job!")
+  console.log("You are too young to vote!");  // false
 }
+
+var year = 2010;
+var age = 2022 - year;
+if (age <= 18) {
+  console.log("You can vote!");
+}
+else {
+  console.log("You are too young to vote!");
+}
+
+var lightSwitch = true;
+if (lightSwitch) {
+  console.log("Light is on!");
+}
+else {
+  console.log("Light is off!");
+}
+
+
+// интерактивная функция (задает вопрос) "confirm"
+// if (confirm("Do you have a car?")) {
+//   if (confirm("Is it red?")) {
+//     alert("You have a red car.");
+//   }
+//   else {
+//     alert("You have a car that is not red.");
+//   } 
+// }
+// else {
+//   if (confirm("Are you planning to have a car?")) {
+//     alert("You are planning to a car.");
+//   }
+//   else {
+//     alert("You don't like cars.");
+//   }
+// }
+
+//var username = prompt("What is your name?", "stranger");
+// if (username === "") {
+//   username = "stranger";
+// }
+//alert("welcome " + username);
+
+var a = prompt("what is the width of the rectangle?");
+var b = prompt("what is the height of the rectangle?");
+alert(a + b);
---
+++ script3.js
@@ -0,0 +1,94 @@
+// home tast
+// var lightSwitch = 1;
+// if (lightSwitch !== 1) {
+//   alert("it is false");
+// }
+// else {
+//   alert("it is true");
+// }
+
+// var tventeenOclock = 1;
+// if (tventeenOclock === 1) {
+//   alert("Now it is true!");
+// }
+// else {
+//   alert("it is wrong!");
+// }
+
+// var asan = 20;
+// if (asan >= 25) {
+//   alert("it is true!");
+// }
+// else {
+//   alert("it is false because Asan is 20 years old!");
+// }
+
+// var iHaveRedmi9C = 1;
+// if (iHaveRedmi9C === 1) {
+//   alert("it is true i have redmi 9c!");
+// }
+// else {
+//   alert("it is false i don't have mobile phone!");
+// }
+
+// var iAmStrong = true;
+// if (iAmStrong) {
+//   alert("i am weak as Halk!");
+// }
+// else  {
+//   alert("i am stong as Superman!");
+// }
+ 
+// if (confirm("Are you eighteen years old?")) {
+//   if (confirm("Is it mean that soon you will become nineteen?")) {
+//     alert("Soon You will be nineteen years old!");
+//   }
+//   else {
+//     alert("You has just become eighteen years old!");
+//   }
+// }
+// else {
+//   if (confirm("Are you seventeen years old?")) {
+//     alert("you are seventeen years old");
+//   }
+//   else {
+//     alert("You are early here!");
+//   }
+// }
+
+// if (confirm("Does the kettle boil?")) {
+//   if (confirm("will it finish own boiling?")) {
+//     alert("it means that the kettle will be heat!");
+//   }
+//   else {
+//     alert("it means that the kettle was very cold.");
+//   }
+// } 
+// else {
+//   if (confirm("will it boil soon?")) {
+//     alert("soon we will be to drink heat a tea!");
+//   }
+//   else {
+//     alert("The kettle didn't switch!");
+//   }
+// }
+ 
+// var username = prompt("What is your name?", "stranger");
+// if (username === "") {
+//   username = "stranger"
+// }
+// alert("Welcome " + username);
+
+// var passward = prompt("Enter your passward here!"); 
+// if (passward === "") {
+//   passward = "000"
+// }
+// alert("It is your passward " + passward);
+
+
+// var country = prompt("where were you born?");
+// if (country === "") {
+//   country = "where is your answer?"
+// }
+// alert("" + country);
+
